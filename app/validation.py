1b19d77bed8c4ad5d2b0df4d57b8c1ce89d29eaf
SaraPalaciosCh <sara.palaciosc@urosario.edu.co>
2025-02-15 08:43:16 -0500

    First Lab - Inputval

---
+++ app/validation.py
@@ -0,0 +1,40 @@
+from _datetime import datetime
+import re
+
+import unicodedata
+
+
+def normalize_input(data):
+    return data
+
+
+# valido el email
+def validate_email(email):
+    email = normalize_input(email)
+    return True
+
+
+# valido la edad
+def validate_dob(dob):
+
+    return True
+
+
+# valido el usuario
+def validate_user(user):
+    return True
+
+
+# valido el dni
+def validate_dni(dni):
+    
+    return True
+
+
+# valido la contraseña
+def validate_pswd(pswd):
+    return True
+
+
+def validate_name(name):
+    return True
